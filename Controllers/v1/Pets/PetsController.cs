1380ba83462c6b564238fbbe0085a4852694ff96
Javier Combita Tellez <javiercombita2014@gmail.com>
2024-10-04 07:38:50 -0500

    separacion de controladores

--- Controllers/v1/Pets/PetsController.cs
+++ Controllers/v1/Pets/PetsController.cs
@@ -8,49 +8,10 @@ namespace RepasoJWT.Controllers.v1.Pets;
 [Route("api/v1/pets")]
 public class PetsController : ControllerBase
 {
-    private readonly IPetRepository servicios;
+    protected readonly IPetRepository servicios;
 
     public PetsController(IPetRepository petRepository)
     {
         servicios = petRepository;
     }
-
-    [HttpGet]
-    public async Task<IActionResult> GetAllPets()
-    {
-        var pets = await servicios.GetAll();
-        if (pets == null || !pets.Any())
-        {
-            return NoContent();
-        }
-
-        return Ok(pets);
-    }
-
-    [HttpGet("{id}")]
-    public async Task<IActionResult> GetPetById(int id)
-    {
-        var pet = await servicios.GetById(id);
-        if (pet == null)
-        {
-            return NotFound();
-        }
-
-        return Ok(pet);
-    }
-
-    [HttpPost]
-    public async Task<IActionResult> AddPet(Pet pet)
-    {
-        await servicios.Add(pet);
-        return Ok("mascota creada");
-    }
-
-    [HttpDelete("{id}")]
-    public async Task<IActionResult> DeletePet(int id)
-    {
-        await servicios.Delete(id);
-        return NoContent();
-    }
-
 }
