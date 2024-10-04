78af60ba7dd567c1b23ce09a6a4adeee6294cd63
Javier Combita Tellez <javiercombita2014@gmail.com>
2024-10-04 08:00:44 -0500

    creacion el metodo que genera JWT

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -6,6 +6,7 @@ namespace RepasoJWT.Data;
 public class ApplicationDbContext : DbContext
 {
     public DbSet<Pet> Pets { get; set; }
+    public DbSet<User> Users { get; set; }
 
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) {}
 }
